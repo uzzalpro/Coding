fa24f7a2ab270700cf82a5801c2db3a4b841fba9
uzzalpro <uzzal.dna@gmail.com>
2022-01-03 17:01:07 +0600

    First committ

--- PdfViewer.py
+++ PdfViewer.py
@@ -1,4 +1,58 @@
 #!/bin/python3
+'''
+
+When a contiguous block of text is selected in a PDF viewer, the selection is highlighted with 
+a blue rectangle.In this PDF viewer, each word is highlighted independently. For example:
+
+There is a list of  26 character heights aligned by index to their letters. 
+For example, 'a' is at index 0 and 'z' is at index 25. There will also be a string. 
+Using the letter heights given, determine the area of the rectangle highlight in (mm^2) 
+assuming all letters are (1mm) wide.
+
+h = [1,3,1,3,1,4,1,3,2,5,5,5,5,1,1,5,5,5,5,2,5,5,5,5,5,7] word = 'tron'
+
+The heights are t=2, o=1,r=1 and n=1. The tallest letter is 2 high and there are 2 letters. 
+The hightlighted area will be 2*4 = 8mm^2 so the answer is 8.
+
+Constraints:
+1 <= h[?], <= 7 where ? is an English lowercase letter.
+word contains no more than 10 letters.
+
+-----------------------
+Sample Input 0
+
+1 3 1 3 1 4 1 3 2 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+abc
+Sample Output 0
+
+9
+Explanation 0
+
+We are highlighting the word abc:
+
+Letter heights are a = 1,b=3 and c=1. The tallest letter, b, is 3mm high. The selection area for this word is 3. 
+1mm.3mm=9mm^2
+
+Note: Recall that the width of each character is 1mm.
+--------------------------------
+Sample Input 1
+
+1 3 1 3 1 4 1 3 2 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 7
+zaba
+Sample Output 1
+
+28
+Explanation 1
+
+The tallest letter in zaba is z at 7mm. The selection area for this word is 4*1mm*7mm=28mm^2.
+
+
+
+
+
+
+
+'''
 
 import math
 import os
